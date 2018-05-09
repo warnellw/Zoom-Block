a3eab43b220042ee9cebf998d4878363d85a6b55
Wesley Warnell <3211186+warnellw@users.noreply.github.com>
2018-05-09 14:15:12 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ZoomBlock
-Zoom Block is a browser extension that disables zooming.
+<h1 align="center">
+    <sub>
+        <img src="icons/red38.png" height="38" width="38" alt="Zoom Block Icon">
+    </sub>
+    Zoom Block
+</h1>
+
+Zoom Block is a browser extension that disables zooming by default preventing unwanted or accidental zooming. Zooming can be re-enable on a per-tab basis by clicking the Zoom Block icon.
+
+## FAQ
+
+**Q: What permissions does Zoom Block require?**\
+A: Zoom Block does not require any permissions. Through careful handling of errors, Zoom Block is able to operate without access to any sensitive information.
+
+**Q: Does Zoom Block use analytics?**\
+A: No. Zoom Block respects its users' privacy. No network requests of any kind are ever made.
+
+**Q: Can Zoom Block "lock" a custom zoom level?**\
+A: The browser extension API does not offer an elegant way to accomplish this. The best option is to change the browser's default zoom level. Chrome's default zoom settings can be found in Settings -> Appearance -> Page Zoom. Zoom Block will then prevent modifications to the set level.
+
+**Q: Why does Zoom Block's icon sometimes turn grey?**\
+A: Some browsers do not allow extensions to modify certain pages (typically internal or settings pages). Zoom Block is therefore unable to prevent zooming on those pages, and the icon will turn grey to indicate such a situation.
+
+**Q: Does Zoom Block work in other browsers?**\
+A: Microsoft Edge and Mozilla Firefox have not yet properly implemented the browser extension zoom API. Once the issues are fixed, Zoom Block will be published to each repective store.
+
+**Q: I found a bug! Where can I submit a report?**\
+A: Please [open an issue](https://github.com/warnellw/ZoomBlock/issues) with a detailed description. Pull requests welcome!
+
+## License
+
+Zoom Block is licensed under [GPLv3](license.txt).
---
+++ ZoomBlock.js
@@ -0,0 +1,142 @@
+/*
+
+    Zoom Block - A browser extension that disables zooming.
+    Copyright (C) 2018 Wesley Warnell
+    https://github.com/warnellw/ZoomBlock
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+'use strict';
+
+(api => {
+    class ZoomBlock {
+        constructor(api) {
+            this.browser = api;
+
+            this.enabledTabs = new Set();
+
+            this.images = new Map()
+                .set('red', {
+                    '19': this.browser.runtime.getURL('icons/red19.png'),
+                    '38': this.browser.runtime.getURL('icons/red38.png')
+                })
+                .set('green', {
+                    '19': this.browser.runtime.getURL('icons/green19.png'),
+                    '38': this.browser.runtime.getURL('icons/green38.png')
+                })
+                .set('black', {
+                    '19': this.browser.runtime.getURL('icons/black19.png'),
+                    '38': this.browser.runtime.getURL('icons/black38.png')
+                });
+        }
+
+        addListeners() {
+            this.browser.browserAction.onClicked.addListener(Tab =>
+                this.iconClick(Tab)
+            );
+            this.browser.runtime.onInstalled.addListener(() => this.init());
+            this.browser.runtime.onStartup.addListener(() => this.init());
+            this.browser.tabs.onRemoved.addListener(tabId =>
+                this.removeEnabled(tabId)
+            );
+            this.browser.tabs.onUpdated.addListener((tabId, changeInfo) =>
+                this.tabUpdated(tabId, changeInfo)
+            );
+        }
+
+        getEnabled(tabId) {
+            return this.enabledTabs.has(tabId);
+        }
+
+        setEnabled(tabId) {
+            this.enabledTabs.add(tabId);
+        }
+
+        removeEnabled(tabId) {
+            this.enabledTabs.delete(tabId);
+        }
+
+        zoomConstructor(mode) {
+            return {
+                mode: mode === false ? 'disabled' : 'automatic',
+                scope: 'per-tab'
+            };
+        }
+
+        updateIcon(tabId, enabled) {
+            let color =
+                enabled === false
+                    ? 'red'
+                    : enabled === true
+                        ? 'green'
+                        : 'black';
+            this.browser.browserAction.setIcon({
+                path: this.images.get(color),
+                tabId: tabId
+            });
+        }
+
+        init() {
+            this.browser.tabs.query({}, results => {
+                results.forEach(result => this.store(result));
+            });
+        }
+
+        store(Tab) {
+            this.setZoomSettings(Tab.id, false, error => {
+                if (error) this.updateIcon(Tab.id, undefined);
+            });
+        }
+
+        iconClick(Tab) {
+            let enabled = !this.getEnabled(Tab.id);
+            this.setZoomSettings(Tab.id, enabled, error => {
+                if (enabled && !error) {
+                    this.setEnabled(Tab.id);
+                } else if (!error) {
+                    this.removeEnabled(Tab.id);
+                }
+                this.updateIcon(
+                    Tab.id,
+                    error ? undefined : enabled ? true : false
+                );
+            });
+        }
+
+        setZoomSettings(tabId, zoomSetting, callback) {
+            this.browser.tabs.setZoomSettings(
+                tabId,
+                this.zoomConstructor(zoomSetting),
+                () => {
+                    callback(this.browser.runtime.lastError || undefined);
+                }
+            );
+        }
+
+        tabUpdated(tabId, changeInfo) {
+            if (changeInfo.status !== 'loading') return;
+            let enabled = this.getEnabled(tabId);
+            this.setZoomSettings(tabId, enabled, error => {
+                //https://bugs.chromium.org/p/chromium/issues/detail?id=30113
+                if (error || enabled) {
+                    this.updateIcon(tabId, error ? undefined : true);
+                }
+            });
+        }
+    }
+
+    new ZoomBlock(api).addListeners();
+})(window.browser || window.chrome);
